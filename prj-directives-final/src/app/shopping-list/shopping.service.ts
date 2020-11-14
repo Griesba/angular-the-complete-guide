30dff2adec4e475a3b4a19e51da5e2bf26c7bed6
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-11-14 15:30:26 +0100

    Create shopping service. closes #1

--- prj-directives-final/src/app/shopping-list/shopping.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping.service.ts
@@ -2,7 +2,7 @@ import {Ingredient} from '../shared/ingredient.model';
 import {EventEmitter} from '@angular/core';
 
 export class ShoppingService {
-  ingredientChangedEvent: EventEmitter<Ingredient[]>;
+  ingredientChangedEvent = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -12,11 +12,16 @@ export class ShoppingService {
     return this.ingredients.slice();
   }
 
-  addIngredients(ingredient: Ingredient) {
+  addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    //since we are returning a copy of ingredient array, an update of the original array is not repecuted on the copy
-    //that is why we have to are using an EventEmiter that will emit changes to array copy.
+    // since we are returning a copy of ingredient array, an update of the original array is not repecuted on the copy
+    // that is why we have to are using an EventEmiter that will emit changes to array copy.
     // another solution is not to slice() the original array
+    this.ingredientChangedEvent.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
     this.ingredientChangedEvent.emit(this.ingredients.slice());
   }
 }
