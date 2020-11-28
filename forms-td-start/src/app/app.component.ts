ebc05a61c01e2f02cbf80fe2abf40d1eb0963d62
griesba <griesba.pat@hotmail.com>
2020-11-28 01:12:52 +0100

    set default value with property binding closes #12

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('formElement') formElement: NgForm;
+  defaultQuestion = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
