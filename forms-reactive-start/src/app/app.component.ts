b6df5d3ae3779f0d4c0e6005250ab48a7ef94fd1
griesba <griesba.pat@hotmail.com>
2020-12-20 17:40:49 +0100

    [reative form] Custom validators closes #21 [reactive form] Create FormArray closes #20

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm: FormGroup;
+  forbiddenUsername = ['Anna', 'Maria', 'Tokyo'];
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenUsernameController.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSumbmit (): void {
     console.log(this.signUpForm);
+  }
+
+  addHobby(): void {
+    const formControl = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(formControl);
+  }
+
+  getControls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+
+  get controls() {
+    // this function has same functionalities with getControls()
+    return (this.signUpForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenUsernameController (control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsername.indexOf(control.value) > -1) {
+      return {'usernameIsForbidden': true};
+    }
+    return null;
   }
 
 }
