fe38e55a1602d73920cc1d832f1fb55a7f3c4169
griesba <griesba.pat@hotmail.com>
2020-12-26 00:44:05 +0100

    adding new ingredient controls

--- prj-directives-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ prj-directives-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -56,4 +56,11 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  addIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+      'name': new FormControl(),
+      'amount': new FormControl()
+    }));
+  }
+
 }
