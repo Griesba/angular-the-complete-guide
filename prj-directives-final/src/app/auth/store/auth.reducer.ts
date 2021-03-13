aee4aaa433ae6e1cfa8326befa4a40ff01ce58d6
griesba <griesba.pat@hotmail.com>
2021-03-13 23:43:26 +0100

    [NgRx] Setting up Auth reducer ans action

--- prj-directives-final/src/app/auth/store/auth.reducer.ts
+++ prj-directives-final/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import {User} from '../user.model';
+import * as fromAuthActions from './auth.actions';
 
 export interface State {
   user: User;
@@ -8,6 +9,20 @@ const initialState: State = {
   user: null
 };
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: fromAuthActions.AuthActions) {
+  switch (action.type) {
+    case 'LOGIN':
+      const newUser = new User(action.payload.email, action.payload.userId, action.payload.token, action.payload.expirationDate);
+      return {
+        ...state,
+        user: newUser
+      };
+    case 'LOGIN':
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
 }
